ea7e8377476882eca32de4a4626fef8cb33f4942
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2021-12-30 23:19:10 -0400

    suspense custom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import TableCustom from "./components/TableCustom";
 
+const MySuspense = ({ children, fallback, isLoading }) => {
+  useEffect(() => {
+    console.log("Suspense.js");
+  }, [isLoading]);
+  return isLoading ? fallback : children;
+};
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    console.log("App.js");
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MySuspense isLoading={isLoading} fallback={<h1>Loading...</h1>}>
+        <TableCustom />
+      </MySuspense>
     </div>
   );
 }
